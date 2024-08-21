efc4ef09535f33b55eb86a22b88ddfaf88e11524
KiJeong-Lim <gijungdduk@naver.com>
2024-08-21 22:27:29 +0900

    update `Aczel.v`

--- theories/Data/Aczel.v
+++ theories/Data/Aczel.v
@@ -8,6 +8,10 @@ Axiom t_isSetoid : isSetoid t.
 
 #[local] Existing Instance t_isSetoid.
 
+Parameter member : t -> t -> Prop.
+
+#[local] Infix "\in" := member : type_scope.
+
 End AczelSig.
 
 Universe Set_u.
@@ -24,7 +28,8 @@ Fixpoint eqTree (lhs : Tree) (rhs : Tree) : Prop :=
   | mkNode cs1 ts1, mkNode cs2 ts2 => (forall c1 : cs1, exists c2 : cs2, eqTree (ts1 c1) (ts2 c2)) /\ (forall c2 : cs2, exists c1 : cs1, eqTree (ts1 c1) (ts2 c2))
   end.
 
-Lemma eqTree_Reflexive
+#[global]
+Instance eqTree_Reflexive
   : Reflexive eqTree.
 Proof.
   intros x. induction x as [csx tsx IH]. simpl. split.
@@ -32,7 +37,8 @@ Proof.
   - intros c1. exists c1. exact (IH _).
 Defined.
 
-Lemma eqTree_Symmetric
+#[global]
+Instance eqTree_Symmetric
   : Symmetric eqTree.
 Proof.
   intros x. induction x as [csx tsx IH], y as [csy tsy]. simpl. intros [x2y y2x]. split.
@@ -40,7 +46,8 @@ Proof.
   - intros c1. pose proof (x2y c1) as [c2 EQ]. exists c2. exact (IH _ _ EQ).
 Defined.
 
-Lemma eqTree_Transitive
+#[global]
+Instance eqTree_Transitive
   : Transitive eqTree.
 Proof.
   intros x. induction x as [csx tsx IH], y as [csy tsy], z as [csz tsz]. simpl. intros [x2y y2x] [y2z z2y]. split.
@@ -56,14 +63,79 @@ Instance Tree_isSetoid : isSetoid Tree :=
 
 Definition t_isSetoid : isSetoid t := Tree_isSetoid.
 
+Lemma eqTree_unfold
+  : eqTree = (fun lhs => fun rhs => lhs == rhs).
+Proof.
+  reflexivity.
+Defined.
+
+Ltac unred_eqTree := rewrite eqTree_unfold in *.
+
 Definition children (x : Tree) : Type@{Set_u} :=
   match x with
   | mkNode cs _ => cs
   end.
 
-Definition childnodes (x : Tree) : forall c : children x, Tree :=
+Definition childnodes {x : Tree} : forall c : children x, Tree :=
   match x with
   | mkNode _ ts => ts
   end.
+
+Definition member (x : Tree) (y : Tree) : Prop :=
+  exists c : children y, x == childnodes c.
+
+Infix "\in" := member : type_scope.
+
+Lemma eqProp_member_member x y z
+  (EQ : x == y)
+  (IN : y \in z)
+  : x \in z.
+Proof.
+  destruct IN as [c EQ']. exists c. transitivity y; [exact EQ | exact EQ'].
+Defined.
+
+Lemma member_eqProp_member x y z
+  (IN : x \in y)
+  (EQ : y == z)
+  : x \in z.
+Proof.
+  destruct IN as [c EQ'], y as [csy tsy], z as [csz tsz]. simpl in *.
+  destruct EQ as [y2z z2y]. pose proof (y2z c) as [c' EQ]. exists c'.
+  simpl. transitivity (tsy c); [exact EQ' | exact EQ].
+Defined.
+
+#[global]
+Add Parametric Morphism
+  : member with signature (eqProp ==> eqProp ==> iff)
+  as member_compatWith_eqTree.
+Proof.
+  intros x1 y1 EQ1 x2 y2 EQ2; split; intros H_IN.
+  - eapply member_eqProp_member; [eapply eqProp_member_member; [eapply eqTree_Symmetric; exact EQ1 | exact H_IN] | exact EQ2].
+  - eapply member_eqProp_member; [eapply eqProp_member_member; [exact EQ1 | exact H_IN] | eapply eqTree_Symmetric; exact EQ2].
+Defined.
+
+#[global]
+Add Parametric Morphism
+  : (Acc member) with signature (eqProp ==> iff)
+  as Acc_member_compatWith_eqTree.
+Proof.
+  intros x y EQ; split; intros [H_inv].
+  - econs. intros z H_IN. eapply H_inv. eapply member_eqProp_member; [exact H_IN | eapply eqTree_Symmetric; exact EQ].
+  - econs. intros z H_IN. eapply H_inv. eapply member_eqProp_member; [exact H_IN | exact EQ].
+Defined.
+
+Lemma member_wf
+  : well_founded member.
+Proof.
+  intros x. induction x as [csx tsx IH]; simpl. econs.
+  intros y [c EQ]. simpl in *. eapply Acc_member_compatWith_eqTree; [exact EQ | eapply IH].
+Defined.
+
+Definition member_rect (P : Tree -> Type)
+  (IND : forall x, (forall y, y \in x -> P y) -> P x)
+  : forall x, P x.
+Proof.
+  exact (@well_founded_induction_type Tree member member_wf P IND).
+Defined.
 
 End Aczel.
