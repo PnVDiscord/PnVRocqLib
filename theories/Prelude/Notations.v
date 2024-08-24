3e3ebfa3f1fe79093c1e6f19642231512072d464
KiJeong-Lim <gijungdduk@naver.com>
2024-08-24 18:01:11 +0900

    add notations

--- theories/Prelude/Notations.v
+++ theories/Prelude/Notations.v
@@ -2,17 +2,25 @@ Require Import PnV.Prelude.SfLib.
 
 Reserved Infix "==" (no associativity, at level 70).
 Reserved Infix "=<" (no associativity, at level 70).
-Reserved Infix "≦" (at level 70, no associativity).
 
-Reserved Infix "≦ᵣ" (at level 70, no associativity).
-Reserved Infix "=ᵣ" (at level 70, no associativity).
-Reserved Infix "<ᵣ" (at level 70, no associativity).
+Reserved Infix "≦ᵣ" (no associativity, at level 70).
+Reserved Infix "=ᵣ" (no associativity, at level 70).
+Reserved Infix "<ᵣ" (no associativity, at level 70).
 
-Reserved Infix "⪵" (at level 70, no associativity).
+Reserved Infix "⪵" (no associativity, at level 70).
+Reserved Infix "≦" (no associativity, at level 70).
+Reserved Infix "≨" (no associativity, at level 70).
+
+Reserved Infix "⊑" (no associativity, at level 70).
 
 Reserved Infix "\in" (no associativity, at level 70).
 Reserved Infix "\subseteq" (no associativity, at level 70).
 
-Reserved Infix "$" (at level 100, right associativity).
-Reserved Infix ">>=" (at level 90, left associativity).
-Reserved Infix ">=>" (at level 45, right associativity).
+Reserved Infix "⊢" (no associativity, at level 70).
+Reserved Infix "⊨" (no associativity, at level 70).
+Reserved Infix "⊬" (no associativity, at level 70).
+Reserved Infix "⊭" (no associativity, at level 70).
+
+Reserved Infix "$" (right associativity, at level 100).
+Reserved Infix ">>=" (left associativity, at level 90).
+Reserved Infix ">=>" (right associativity, at level 45).
