d3ac7441a306984f3c9ea16a115616aba8187125
KiJeong-Lim <gijungdduk@naver.com>
2024-10-17 00:06:26 +0900

    remove old source code

--- theories/Math/Ordinal.v
+++ theories/Math/Ordinal.v
@@ -4,42 +4,33 @@ Require Import PnV.Prelude.AC.
 Require Import PnV.Math.OrderTheory.
 Require Import PnV.Data.Aczel.
 
+#[local] Infix "\in" := member : type_scope.
+#[local] Infix "\subseteq" := isSubsetOf : type_scope.
+
 Module Ord.
 
+Variant trichotomy (x : Tree) (y : Tree) : Prop :=
+  | trichotomy_lt
+    (LT : x \in y)
+    : trichotomy x y
+  | trichotomy_eq
+    (EQ : x == y)
+    : trichotomy x y
+  | trichotomy_gt
+    (GT : y \in x)
+    : trichotomy x y.
+
+Variant isOrdinal (alpha : Tree) : Prop :=
+  | isOrdinal_intro
+    (TRANS : isTransitiveSet alpha)
+    (TOTAL : forall x, forall y, x \in alpha -> y \in alpha -> trichotomy x y)
+    : isOrdinal alpha.
+
 Record t : Type :=
-  Mk
-  { asSet : Tree
-  ; isOrd : isOrdinal asSet
-  }.
-
-Definition empty : Ord.t :=
-  Mk empty empty_isOrdinal.
-
-Definition succ (x : Ord.t) : Ord.t :=
-  Mk (succ (asSet x)) (succ_isOrdinal (asSet x) (isOrd x)).
-
-Definition limit {A : Type@{Set_u}} (x : A -> Ord.t) : Ord.t :=
-  Mk (indexed_union A (fun i => asSet (x i))) (indexed_union_isOrdinal (fun i => asSet (x i)) (fun i => isOrd (x i))).
+  Mk { asSet : Tree; isOrd : isOrdinal asSet }.
 
 End Ord.
 
 Notation asSet := Ord.asSet.
 Notation isOrd := Ord.isOrd.
 Notation MkOrd o o_ord := (Ord.Mk o o_ord).
-
-#[global]
-Instance Ord_isSetoid : isSetoid Ord.t :=
-  { eqProp lhs rhs := lhs.(asSet) =ᵣ rhs.(asSet)
-  ; eqProp_Equivalence := relation_on_image_liftsEquivalence rEq_asSetoid.(eqProp_Equivalence) asSet
-  }.
-
-#[global]
-Instance Ord_isProset : isProset Ord.t :=
-  { leProp lhs rhs := lhs.(asSet) ≦ᵣ rhs.(asSet)
-  ; Proset_isSetoid := Ord_isSetoid
-  ; leProp_PreOrder := relation_on_image_liftsPreOrder rLe_PreOrder asSet
-  ; leProp_PartialOrder := relation_on_image_liftsPartialOrder rLe_PartialOrder asSet
-  }.
-
-#[local] Infix "\in" := member : type_scope.
-#[local] Infix "\subseteq" := isSubsetOf : type_scope.
