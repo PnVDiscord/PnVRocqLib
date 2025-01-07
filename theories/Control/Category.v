4347a3cafc9aa664a48f979555a8c230ddb27a55
KiJeong-Lim <gijungdduk@naver.com>
2025-01-07 19:22:19 +0900

    `commutes`

--- theories/Control/Category.v
+++ theories/Control/Category.v
@@ -43,6 +43,10 @@ Class isCovariantFunctor@{u1 v1 u2 v2} (Dom : isCategory@{u1 v1}) (Cod : isCateg
 
 Section LAW.
 
+#[universes(polymorphic=yes)]
+Definition commutes@{d c} {Src : Type@{d}} {Tgt : Type@{c}} (REL_Src : Src -> Src -> Prop) (REL_Tgt : Tgt -> Tgt -> Prop) (MAP : Src -> Tgt -> Prop) : Prop :=
+  forall x : Src, forall y' : Tgt, (exists x' : Src, REL_Src x x' /\ MAP x' y') <-> (exists y : Tgt, MAP x y /\ REL_Tgt y y').
+
 Class isLawfulCategory (CAT : isCategory) {SETOID : forall Dom : CAT.(ob), forall Cod : CAT.(ob), isSetoid (CAT.(hom) Dom Cod)} : Prop :=
   { compose_assoc {A : CAT.(ob)} {B : CAT.(ob)} {C : CAT.(ob)} {D : CAT.(ob)} (h : CAT.(hom) C D) (g : CAT.(hom) B C) (f : CAT.(hom) A B)
     : compose h (compose g f) == compose (compose h g) f
@@ -57,9 +61,8 @@ Class isLawfulCovariantFunctor {Dom : isCategory} {Cod : isCategory} {SETOID : f
     : fmap_hom (Dom.(@compose) A B C g f) == compose (fmap_hom g) (fmap_hom f)
   ; fmap_id {A : Dom.(ob)}
     : fmap_hom (Dom.(@id) A) == id
-  ; fmap_lifts_ext_eq {A : Dom.(ob)} {B : Dom.(ob)} (f : Dom.(hom) A B) (f' : Dom.(hom) A B)
-    (f_EQ : f == f')
-    : fmap_hom f == fmap_hom f'
+  ; fmap_comm {A : Dom.(ob)} {B : Dom.(ob)}
+    : commutes eqProp eqProp (fun f : Dom.(hom) A B => fun fmap_f : Cod.(hom) (fmap_ob A) (fmap_ob B) => fmap_hom f == fmap_f)
   }.
 
 End LAW.
@@ -78,7 +81,7 @@ Instance Hask@{u v} : CAT.isCategory@{u v} :=
 
 #[local]
 Instance Setoid_on_Hask (Dom : Hask.(CAT.ob)) (Cod : Hask.(CAT.ob)) : isSetoid (Hask.(CAT.hom) Dom Cod) :=
-  pi_isSetoid (fun _ => mkSetoid_from_eq).
+  pi_isSetoid (fun _ : Dom => mkSetoid_from_eq).
 
 #[global]
 Instance Hask_isLawfulCategory
@@ -97,11 +100,15 @@ Instance Functor_CAT_Functor@{u1 v1 u2 v2} (F : Type@{v1} -> Type@{v2}) {F_isFun
 Instance Functor_CAT_Functor_good {F : Type -> Type} {F_isFunctor : isFunctor F} {F_isSetoid1 : isSetoid1 F}
   (FUNCTOR_LAWS : FunctorLaws F (FUNCTOR := F_isFunctor) (SETOID1 := F_isSetoid1))
   : CAT.isLawfulCovariantFunctor (SETOID := Setoid_on_Hask) (Functor_CAT_Functor F) (liftSETOID := fun X : Type => fun Y : Type => fun _ : isSetoid (X -> Y) => pi_isSetoid (fun _ : F X => liftSetoid1 (isSetoid1 := F_isSetoid1) Y mkSetoid_from_eq)).
-Proof.
+Proof with eauto with *.
   destruct FUNCTOR_LAWS. split; cbn in fmap_compose, fmap_id |- *; intros.
   - eapply fmap_compose.
   - eapply fmap_id.
-  - eapply fmap_lifts_ext_eq. exact f_EQ.
+  - red. intros f fmap_f. split.
+    + intros (f'&f_EQ&fmap_f_EQ). exists fmap_f. split...
+      intros x. rewrite <- fmap_f_EQ with (x := x). eapply fmap_lifts_ext_eq...
+    + intros (fmap_f'&fmap_f_EQ&fmap_f_EQ'). exists f. split...
+      intros x. rewrite -> fmap_f_EQ with (x := x)...
 Qed.
 
 End HASK.
