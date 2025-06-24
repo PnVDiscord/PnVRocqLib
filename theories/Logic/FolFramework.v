e477a94d7f8563f64cd4e7b7e246a46a50ddb841
KiJeong-Lim <gijungdduk@naver.com>
2025-06-24 19:32:31 +0900

    Add an example. (signature of ZFC)

--- theories/Logic/FolFramework.v
+++ theories/Logic/FolFramework.v
@@ -6,3 +6,21 @@ Require Import PnV.Logic.BasicFol.
 Require Import PnV.Logic.BasicFol2.
 Require Import PnV.Logic.HilbertFol.
 Require Import PnV.Logic.HilbertFol2.
+
+Section EXAMPLE1.
+
+Variant L_in_relation_symbols : Set :=
+  | symbol_IN : L_in_relation_symbols.
+
+Definition L_in : language :=
+  {|
+    function_symbols := Empty_set;
+    relation_symbols := L_in_relation_symbols;
+    constant_symbols := Empty_set;
+    function_arity_table := Empty_set_rect (fun _ => nat);
+    relation_arity_table := fun _ => 2%nat;
+    function_arity_gt_0 := Empty_set_ind _;
+    relation_arity_gt_0 := fun _ => @le_S 1 1 (@le_n 1);
+  |}.
+
+End EXAMPLE1.
