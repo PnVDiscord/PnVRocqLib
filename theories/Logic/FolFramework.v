f325ec11f6f34fa38c9d5a742fccb97d794b6e13
KiJeong-Lim <gijungdduk@naver.com>
2025-06-24 19:41:12 +0900

    .

--- theories/Logic/FolFramework.v
+++ theories/Logic/FolFramework.v
@@ -1,7 +1,4 @@
 Require Import PnV.Prelude.Prelude.
-Require Import PnV.Prelude.ConstructiveFacts.
-Require Import PnV.Math.ThN.
-Require Import PnV.Data.Vector.
 Require Import PnV.Logic.BasicFol.
 Require Import PnV.Logic.BasicFol2.
 Require Import PnV.Logic.HilbertFol.
