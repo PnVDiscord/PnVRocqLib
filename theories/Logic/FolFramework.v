769e390d51db520f10c63fb2e4d764ae0cd9657a
KiJeong-Lim <gijungdduk@naver.com>
2025-06-25 16:08:19 +0900

    fix formula notation

--- theories/Logic/FolFramework.v
+++ theories/Logic/FolFramework.v
@@ -22,6 +22,20 @@ Declare Scope subst_scope.
 Notation "'$' EXPR '$'" := EXPR : frm_scope.
 Notation "'$' EXPR '$'" := (EXPR : frm _).
 
+Bind Scope frm_scope with frm.
+Notation "`[ s ] p" := (subst_frm s p) (s custom subst_view at level 10, p custom frm_view at level 5, in custom frm_view at level 5, format "`[ s ] p").
+Notation "'⊥'" := (Bot_frm) (in custom frm_view at level 0).
+Notation "t1 '=' t2" := (Eqn_frm t1 t2) (in custom frm_view at level 6).
+Notation "'¬' p" := (Neg_frm p) (in custom frm_view at level 7).
+Notation "'(∀' x ')' p" := (All_frm x p) (x constr at level 0, p custom frm_view at level 7, in custom frm_view at level 7).
+Notation "'(∃' x ')' p" := (Exs_frm x p) (x constr at level 0, p custom frm_view at level 7, in custom frm_view at level 7).
+Notation "p '∧' q" := (Con_frm p q) (no associativity, in custom frm_view at level 8).
+Notation "p '∨' q" := (Dis_frm p q) (no associativity, in custom frm_view at level 9).
+Notation "p '→' q" := (Imp_frm p q) (no associativity, in custom frm_view at level 10).
+Notation "p '↔' q" := (Iff_frm p q) (no associativity, in custom frm_view at level 10).
+Notation "p" := p (p ident, in custom frm_view at level 0).
+Notation "( p )" := p (p custom frm_view at level 10, no associativity, in custom frm_view at level 0).
+
 Bind Scope trm_scope with trm.
 Notation "`[ s ] t" := (subst_trm s t) (s custom subst_view at level 10, t custom trm_view at level 5, in custom trm_view at level 5, format "`[ s ] t") : trm_scope.
 Notation "'V' x" := (Var_trm x) (x constr at level 0, in custom trm_view at level 0).
@@ -36,20 +50,6 @@ Notation "[ ]" := (O_trms) (in custom trms_view at level 0) : trms_scope.
 Notation "t :: ts" := (S_trms _ t ts) (t custom trm_view, ts custom trms_view, in custom trms_view at level 0) : trms_scope.
 Notation "ts" := ts (ts ident, in custom trms_view at level 0).
 Notation "( ts )" := ts (ts custom trms_view at level 5, no associativity, in custom trms_view at level 0).
-
-Bind Scope frm_scope with frm.
-Notation "`[ s ] p" := (subst_frm s p) (s custom subst_view at level 10, p custom frm_view at level 5, in custom frm_view at level 5, format "`[ s ] p").
-Notation "'⊥'" := (Bot_frm) (in custom frm_view at level 0).
-Notation "t1 '=' t2" := (Eqn_frm t1 t2) (in custom frm_view at level 6).
-Notation "'¬' p" := (Neg_frm p) (in custom frm_view at level 7).
-Notation "'(∀' x ')' p" := (All_frm x p) (x constr at level 0, in custom frm_view at level 7).
-Notation "'(∃' x ')' p" := (Exs_frm x p) (x constr at level 0, in custom frm_view at level 7).
-Notation "p '∧' q" := (Con_frm p q) (no associativity, in custom frm_view at level 8).
-Notation "p '∨' q" := (Dis_frm p q) (no associativity, in custom frm_view at level 9).
-Notation "p '→' q" := (Imp_frm p q) (no associativity, in custom frm_view at level 10).
-Notation "p '↔' q" := (Iff_frm p q) (no associativity, in custom frm_view at level 10).
-Notation "p" := p (p ident, in custom frm_view at level 0).
-Notation "( p )" := p (p custom frm_view at level 10, no associativity, in custom frm_view at level 0).
 
 Bind Scope subst_scope with subst.
 Notation "s2 ∘ s1" := (subst_compose s1 s2) (right associativity, in custom subst_view at level 4) : subst_scope.
@@ -74,10 +74,5 @@ Definition L_in : language :=
     function_arity_gt_0 := Empty_set_ind _;
     relation_arity_gt_0 := fun _ => (@le_S 1 1 (@le_n 1));
   |}.
-
-Example foo (x : ivar) (t : trm L_in) (p : frm L_in)
-  (x_eq_t : Var_trm x = t)
-  : $`[t / x] p$ = p.
-Admitted.
 
 End Example1.
