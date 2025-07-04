1b2cd8f79d0216e8d71fc6c9ab95eeec0921a6b1
KiJeong-Lim <gijungdduk@naver.com>
2025-07-04 10:39:47 +0900

    TODO

--- theories/System/Lambda1.v
+++ theories/System/Lambda1.v
@@ -4,66 +4,90 @@ Require Import PnV.System.P.
 Declare Scope typ_scope.
 Delimit Scope typ_scope with typ.
 
+Reserved Notation "Gamma '⊢' M '⦂' A" (at level 70, no associativity).
+
 Module ChurchStyleStlc.
 
-  Reserved Notation "Gamma '⊢' M '⦂' A" (at level 70, no associativity).
+#[projections(primitive)]
+Record language : Type :=
+  { basic_types : Set
+  ; constants : Set
+  }.
 
-  #[projections(primitive)]
-  Record language : Type :=
-    { basic_types : Set
-    ; constants : Set
-    }.
+Inductive typ (L : language) : Set :=
+  | bty (B : L.(basic_types)) : typ L
+  | arr (D : typ L) (C : typ L) : typ L.
 
-  Inductive typ (L : language) : Set :=
-    | bty (B : L.(basic_types)) : typ L
-    | arr (D : typ L) (C : typ L) : typ L.
+#[global] Bind Scope typ_scope with typ.
+#[global] Notation "D -> C" := (@arr _ D C) : typ_scope.
 
-  #[global] Bind Scope typ_scope with typ.
-  #[global] Notation "D -> C" := (@arr _ D C) : typ_scope.
+Class signature (L : language) : Set :=
+  type_of_constant (c : L.(constants)) : typ L.
 
-  Class signature (L : language) : Set :=
-    type_of_constant (c : L.(constants)) : typ L.
+Section STLC.
 
-  Section STLC.
+Context {L : language}.
 
-  Context {L : language}.
+#[local] Notation typ := (typ L).
 
-  #[local] Notation typ := (typ L).
+Inductive trm : Set :=
+  | Var_trm (x : name)
+  | App_trm (e1 : trm) (e2 : trm)
+  | Lam_trm (y : name) (ty : typ) (e1 : trm)
+  | Con_trm (c : L.(constants)).
 
-  Inductive trm : Set :=
-    | Var_trm (x : name)
-    | App_trm (e1 : trm) (e2 : trm)
-    | Lam_trm (y : name) (ty : typ) (e1 : trm)
-    | Con_trm (c : L.(constants)).
+Section FreeVars.
 
-  Section TypingRule.
+(* TODO *)
 
-  Definition ctx : Set :=
-    list (name * typ).
+End FreeVars.
 
-  Context {Sigma : signature L}.
+Section Substitution.
 
-  Inductive syntactic_typing_rule (Gamma : ctx) : trm -> typ -> Prop :=
-    | Var_typ (x : name) (ty : typ)
-      (LOOKUP : L.lookup x Gamma = Some ty)
-      : Gamma ⊢ Var_trm x ⦂ ty
-    | App_typ (e1 : trm) (e2 : trm) (ty1 : typ) (ty2 : typ)
-      (TYP1 : Gamma ⊢ e1 ⦂ (ty1 -> ty2)%typ)
-      (TYP2 : Gamma ⊢ e2 ⦂ ty1)
-      : Gamma ⊢ (App_trm e1 e2) ⦂ ty2
-    | Lam_typ (y : name) (e1 : trm) (ty1 : typ) (ty2 : typ)
-      (TYP1 : (y, ty1) :: Gamma ⊢ e1 ⦂ ty2)
-      : Gamma ⊢ Lam_trm y ty1 e1 ⦂ (ty1 -> ty2)%typ
-    | Con_typ (c : L.(constants))
-      : Gamma ⊢ Con_trm c ⦂ type_of_constant (signature := Sigma) c
-    where "Gamma '⊢' M '⦂' A" := (syntactic_typing_rule Gamma M A) : type_scope.
+(* TODO *)
 
-  End TypingRule.
+End Substitution.
 
-  End STLC.
+Section alphaEquivalence.
 
-  #[global] Arguments trm : clear implicits.
-  #[global] Arguments ctx : clear implicits.
+(* TODO *)
+
+End alphaEquivalence.
+
+Section TypingRule.
+
+Definition ctx : Set :=
+  list (name * typ).
+
+Context {Sigma : signature L}.
+
+Inductive syntactic_typing_rule (Gamma : ctx) : trm -> typ -> Prop :=
+  | Var_typ (x : name) (ty : typ)
+    (LOOKUP : L.lookup x Gamma = Some ty)
+    : Gamma ⊢ Var_trm x ⦂ ty
+  | App_typ (e1 : trm) (e2 : trm) (ty1 : typ) (ty2 : typ)
+    (TYP1 : Gamma ⊢ e1 ⦂ (ty1 -> ty2)%typ)
+    (TYP2 : Gamma ⊢ e2 ⦂ ty1)
+    : Gamma ⊢ (App_trm e1 e2) ⦂ ty2
+  | Lam_typ (y : name) (e1 : trm) (ty1 : typ) (ty2 : typ)
+    (TYP1 : (y, ty1) :: Gamma ⊢ e1 ⦂ ty2)
+    : Gamma ⊢ Lam_trm y ty1 e1 ⦂ (ty1 -> ty2)%typ
+  | Con_typ (c : L.(constants))
+    : Gamma ⊢ Con_trm c ⦂ type_of_constant (signature := Sigma) c
+  where "Gamma '⊢' M '⦂' A" := (syntactic_typing_rule Gamma M A) : type_scope.
+
+End TypingRule.
+
+Section MetaTheory.
+
+(* TODO *)
+
+End MetaTheory.
+
+End STLC.
+
+#[global] Arguments trm : clear implicits.
+#[global] Arguments ctx : clear implicits.
 
 End ChurchStyleStlc.
 
