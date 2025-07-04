4db28bb8809c042a8bdb80dd1b9616199509f5c6
KiJeong-Lim <gijungdduk@naver.com>
2025-07-04 09:42:43 +0900

    STLC

--- theories/System/Lambda1.v
+++ theories/System/Lambda1.v
@@ -1,13 +1,38 @@
 Require Import PnV.Prelude.Prelude.
+Require Import PnV.System.P.
 
-Module ULC.
+Declare Scope typ_scope.
 
-Inductive trm {signature : Set} : Set :=
-  | Idx (i : nat)
-  | App (t1 : @trm signature) (t2 : @trm signature)
-  | Lam (t1 : @trm signature)
-  | Sym (s : signature).
+Module ChurchStyleStlc.
 
-#[global] Arguments trm : clear implicits.
+  #[projections(primitive)]
+  Record language : Type :=
+    { basic_types : Set
+    ; constants : Set
+    }.
 
-End ULC.
+  Inductive typ (L : language) : Set :=
+    | bty (B : L.(basic_types))
+    | arr (D : typ L) (C : typ L) : typ L.
+
+  #[global] Bind Scope typ_scope with typ.
+  #[global] Notation "D -> C" := (arr D C) : typ_scope.
+
+  Class signature (L : language) : Set :=
+    type_of_constant (c : L.(constants)) : typ L.
+
+  Section STLC.
+
+  Context {L : language}.
+
+  Inductive trm : Set :=
+    | Var_trm (x : name)
+    | App_trm (e1 : trm) (e2 : trm)
+    | Lam_trm (y : name) (ty : typ L) (e1 : trm)
+    | Con_trm (c : L.(constants)).
+
+  End STLC.
+
+End ChurchStyleStlc.
+
+#[global] Coercion ChurchStyleStlc.bty : ChurchStyleStlc.basic_types >-> ChurchStyleStlc.typ.
