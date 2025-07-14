1f9d2028ebd34f9902a6cc6eebd2633d38bd533e
KiJeong-Lim <gijungdduk@naver.com>
2025-07-14 23:38:55 +0900

    Quot.

--- theories/Index/Index.v
+++ theories/Index/Index.v
@@ -25,6 +25,7 @@ Require Import PnV.Prelude.AC.
 Require Import PnV.Prelude.ClassicalFacts.
 Require Import PnV.Prelude.ConstructiveFacts.
 Require Import PnV.Prelude.Notations.
+Require Import PnV.Prelude.Quot.
 Require Import PnV.Prelude.Prelude.
 Require Import PnV.Prelude.SfLib.
 Require Import PnV.System.BasicITreeTh.
